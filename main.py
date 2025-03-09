fd331731190e9c947df4e08c89675464999450c1
Kessel113 <andreidanielcodreanu@gmail.com>
2025-03-09 11:54:52 +0200

    add menu

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 from nasa_source import get_current_sun_data
 from entropy import get_entropy
+from local_source import get_images, cleanup
+
+#def generate():
+#    num = 0
+#
+#def exit():
+#    cleanup()
+#
+#def main():
+#    print("Random number generator")
+#    run = True
+#    while run:
+#        print("Choose an option:\n")
+#        print("1. Generate random number\n")
+#        print("2. Exit\n")
+#        choice = input("Enter choice: ")
+#        choice = int(choice)
+#        if choice == 1:
+#            generate()
+#        elif choice == 2:
+#            exit()
+#            run = False
+#
+#
+#
+#if __name__ == "__main__":
+#    main()
 
 #get_current_sun_data()
-entropy = get_entropy("./images/20250308_092634_4096_0171.jpg")
-print(entropy)
+#img_list_test = get_images()
+#print("Images found: ", img_list_test)
+#cleanup()
+#entropy = get_entropy("./images/20250309_025609_4096_211193171n.jpg")
+#print(entropy)
 
 # test entropies:
 # HMI171 = 6.5415983
