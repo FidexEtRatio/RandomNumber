9d6de1729c5d044e7821120a9bb75f97b6922447
Kessel <andreidanielcodreanu@gmail.com>
2025-03-14 22:36:00 +0200

    add random number generator

---
+++ generator.py
@@ -0,0 +1,20 @@
+from system_entropy import get_hardware_seed
+import hashlib
+
+def generate(base, seed, beg, end):
+    base = base.encode() if isinstance(base, str) else base
+    seed = seed.encode() if isinstance(seed, str) else seed
+
+    hw_seed = str(get_hardware_seed()).encode()
+    rand = hashlib.sha3_512(base + hashlib.sha3_512(seed + hw_seed).digest()).hexdigest()
+
+    rounds = 3
+    while rounds != 0:
+        hw_seed = str(get_hardware_seed()).encode()
+        rand += hashlib.sha3_512(base + hashlib.sha3_512(seed + hw_seed).digest()).hexdigest()
+        rounds -= 1
+    rand = int(rand, 16)
+    bit_len = (rand % (2048 - 128 + 1)) + 128
+    rand = rand & ((1 << bit_len) - 1)
+    rand = rand % (end - beg + 1) + beg
+    return rand
