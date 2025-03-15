bdbd66f714cc9c6cf8cc5719caaadfec568582f6
Kessel <andreidanielcodreanu@gmail.com>
2025-03-15 10:59:26 +0200

    modify numbers in runner script

--- runner.py
+++ runner.py
@@ -3,8 +3,10 @@ import subprocess
 def main():
     script_to_run = "main.py"
 
-    for i in range(1000):
-        print(f"Execution {i + 1}/1000...")
+    for i in range(2000):
+        print(f"Execution {i + 1}/2000...")
+        with open('executions_count.txt', 'a') as file:
+            print(str(i) + " execution...\n")
         subprocess.run(["python3", script_to_run], check=True)
 
 if __name__ == "__main__":
