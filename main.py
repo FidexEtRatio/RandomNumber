f2e30e1c7f57b706fead4a8db062f029a89733d8
Kessel <andreidanielcodreanu@gmail.com>
2025-03-15 07:19:55 +0200

    convert main into a worker

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from generator import generate
 def main():
     base = get_base()
     count = 1
-    for i in range(30):
-        with open('numbers.txt', 'a') as file:
+    for i in range(100):
+        with open('numbers.txt', 'ab') as file:
             rand_num = generate(base, get_seed(), 1, 500, count)
             count += 1
-            file.write(str(rand_num) + ", ")  # Write the number to the file as a string
-
+            file.write(rand_num.to_bytes(2, byteorder="big"))
+            
 if __name__ == "__main__":
     main()
