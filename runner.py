e4dc118f108aae408b92d7278dfda1a1b115ad1b
Kessel <andreidanielcodreanu@gmail.com>
2025-03-15 11:07:19 +0200

    fixe file verbose for executions

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ def main():
     for i in range(2000):
         print(f"Execution {i + 1}/2000...")
         with open('executions_count.txt', 'a') as file:
-            print(str(i) + " execution...\n")
+            file.write(str(i + 1) + " execution...\n")
         subprocess.run(["python3", script_to_run], check=True)
 
 if __name__ == "__main__":
