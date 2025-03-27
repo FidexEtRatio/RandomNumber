27267b085a782d3b5e53932230df6fbb344c3014
Kessel <andreidanielcodreanu@gmail.com>
2025-03-27 15:47:12 +0200

    add base_data.py

---
+++ base_data.py
@@ -0,0 +1,11 @@
+import numpy
+
+class BaseData:
+    def __init__(self, data):
+        self.data = data
+        self.pos = 0
+
+    def get_base(self):
+        base = self.data[self.pos:self.pos+20]
+        self.pos = (self.pos + 20) % len(self.data)
+        return base
