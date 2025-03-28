adff95c9391cbba6dc2bbde8fbc0fbe8e8892776
Kessel113 <andreidanielcodreanu@gmail.com>
2025-03-28 18:10:32 +0200

    remove bit rotation

--- proc_img.py
+++ proc_img.py
@@ -4,37 +4,6 @@ from local_img_source import get_images, cleanup
 from nasa_source import get_current_sun_data
 import numpy
 
-def rotate_right(n, k):
-    if n == 0 or n == 1:
-        return n
-    
-    msb = get_msb(n)
-    k %= msb
-    mask = create_mask(n)
-    k_tmp = k
-    while k_tmp != 0:
-        mask <<= 1
-        k_tmp -= 1
-    rot = (~mask) & n
-    rot <<= msb - k + 1
-    rot |= n >> k
-    return rot
-    
-def get_msb(n):
-    pos = 0
-    while n != 1:
-        n >>= 1
-        pos += 1
-    return pos
-
-def create_mask(n):
-    mask = 0b1
-    while n > 1:
-        mask <<= 1
-        mask |= 1
-        n >>= 1
-    return mask
-
 def xor_arrays(arr1, arr2):
     return [int(x) & 0xFFFFFFFF ^ int(y) & 0xFFFFFFFF for x, y in zip(arr1, arr2)]
 
