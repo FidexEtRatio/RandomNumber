06d50d06a67b909af41c698f2a5d9b8e02b66fdb
Kessel <andreidanielcodreanu@gmail.com>
2025-04-02 12:50:17 +0300

    update base data every 20 min

--- base_data.py
+++ base_data.py
@@ -1,29 +1,25 @@
 from bernstein import djb2
+import time
 
 class BaseData:
     def __init__(self, data):
         self.data = data
         self.pos = 0
-        self.round = 0
-        self.finish = False
+        self.last_run = 0
 
     def get_base(self):
         base = self.data[self.pos:self.pos+20]
         self.pos = (self.pos + 20) % len(self.data)
-        if self.pos + 20 >= len(self.data):
-            self.round += 1
-            if self.round == 2:
-                self.finish = True
         return djb2(base, 60000)
     
     def about_to_finish(self):
-        return self.finish
+        current_time = time.time()
+        return current_time - self.last_run >= 1200
     
     def update_data(self, data):
         self.data = data
         self.pos = 0
-        self.round = 0
-        self.finish = False
+        self.last_run = 0
 
     def get_len(self):
         return len(self.data)
